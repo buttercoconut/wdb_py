7b8ebfdaf48e218e01458a72de919cbb24c29660
유태웅 <ytw2854@gmail.com>
2019-06-25 10:08:33 +0900

    BTrees package add

--- crud/create.py
+++ crud/create.py
@@ -1,9 +1,14 @@
 import os, sys
-import glob
 import json
+
+from BTrees.OOBTree import OOBTree
+t = OOBTree()
 
 dir = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(dir, '../'))
+
+from crud import read
+R = read.Read()
 
 from conf import config
 conf = config.defult
@@ -34,17 +39,19 @@ class Create(object):
             f = open(pathlink + "/_index", "r")
             idx = f.read()
             f.close()
-            idx = int(idx) + 1
         except:
-            idx = int(idx) + 1
+            pass
         finally:
+            idx = int(idx) + 1
             f = open(pathlink + "/_index", "w")
             f.write(str(idx))
             f.close()
 
-        data_dict = dict(json.loads(data))
+        t.update(json.loads(data))
+        total_data = R.read_all(db, tb)
 
-        for key in data_dict.keys():
-            f = open(pathlink + "/" + key, "a")
-            f.write(str(idx) + "@@@" + str(data_dict[key]) + "$$$")
-            f.close()
+        for key in t.keys():
+            insert_data = {idx: t[key]}
+            with open(pathlink + "/" + key, "w") as outfile:
+                insert_data.update(total_data[key])
+                json.dump(insert_data, outfile)
--- crud/read.py
+++ crud/read.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 import os, sys
-import glob 
+import glob
+from BTrees.OOBTree import OOBTree
+t = OOBTree()
+import json
 
 dir = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(dir, '../'))
@@ -17,10 +20,8 @@ class Read(object):
         self.data = ""
 
     def read_all(self, db, tb):
-        result = {}
 
         self.storePath = conf['db_store_path']
-
         pathlink = self.storePath + "/" + db + "/" + tb
 
         datalist = glob.glob(pathlink + "/*")
@@ -28,51 +29,32 @@ class Read(object):
 
         for datapath in datalist:
 
-            f = open(datapath, "r")
-            low_data = f.read()
             try:
-                result[datapath.split('/')[-1]]
+                with open(datapath, "r") as readfile:
+                    data = json.load(readfile)
+                    t[datapath.split('/')[-1]] = data
             except:
-                result[datapath.split('/')[-1]] = {}
-            finally:
-                data_sep = low_data.split('$$$')
-                for each in data_sep:
-                    try:
-                        result[datapath.split('/')[-1]][each.split('@@@')[0]] = each.split('@@@')[1]
-                    except IndexError:
-                        pass
-            f.close()
+                pass
 
-        return result
+        return t
 
     # DB내의 특정테이블의 특정 key값의 모든 데이터를 조회
-    def read_keyword(self, db, tb, keyword):
-        result = {}
+    def read_keyword(self, db, tb, key):
 
         self.storePath = conf['db_store_path']
         pathlink = self.storePath + "/" + db + "/" + tb
 
         try:
-            datapath = pathlink + "/" + keyword
+            datapath = pathlink + "/" + key
 
-            f = open(datapath, "r")
-            low_data = f.read()
-            try:
-                result[datapath.split('/')[-1]]
-            except:
-                result[datapath.split('/')[-1]] = {}
-            finally:
-                data_sep = low_data.split('$$$')
-                for each in data_sep:
-                    try:
-                        result[datapath.split('/')[-1]][each.split('@@@')[0]] = each.split('@@@')[1]
-                    except IndexError:
-                        pass
-            f.close()
+            with open(datapath, "r") as readfile:
+                data = json.load(readfile)
+                t[datapath.split('/')[-1]] = data
+
         except:
             pass
 
-        return result
+        return t
 
     # DB내의 특정테이블의 특정 value값의 모든 데이터를 조회
     def read_value(self, db, tb, value):
@@ -122,6 +104,7 @@ class Read(object):
                                 result[value][each.split('@@@')[0]][datapath.split('/')[-1]] = each.split('@@@')[1]
                     except IndexError:
                         pass
+
             f.close()
 
         return result
